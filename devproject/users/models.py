3b11acc47e8d35ef15f32f3f9ba06c6a4114b41d
rago7 <gopi.edu7@gmail.com>
2022-04-29 09:51:30 +0530

    signals

--- devproject/users/models.py
+++ devproject/users/models.py
@@ -5,6 +5,8 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+from django.db.models.signals import post_save
+
 # Create your models here.
 
 class Profile(models.Model):
